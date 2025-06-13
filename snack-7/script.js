acb37abaf57785667be7e35849b19cf4ac00c79e
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-13 17:59:03 +0200

    feat: finish snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const id2student = students.find(student => student.id === 2);
+
+console.log(id2student);

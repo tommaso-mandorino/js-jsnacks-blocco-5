054dc613c2b65d6a1c8642fd249b0ae1010ca96a
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-13 17:19:18 +0200

    feat: finish snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorNames = posts.map(author => author.author);
+
+console.log(authorNames);

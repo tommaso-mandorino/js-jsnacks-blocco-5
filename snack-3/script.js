9712b0c02c77ac12abc746b4174e8227fe35ac1c
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-13 17:16:00 +0200

    feat: finish snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,7 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const increasedNumbers = numbers.map(number => number + 1);
+
+console.log(increasedNumbers);
